161e6d9cd26f53ae6f4013d14e34eab5883ca16d
SKrauth <spkrauth@gmail.com>
2015-10-25 00:26:25 -0400

    Basic home page

--- Public/js/controllers/HomeController.js
+++ Public/js/controllers/HomeController.js
@@ -4,18 +4,13 @@ app.controller("HomeController", function($scope){
     $scope.imageLeft = "";
     $scope.logo = "";
     
-    $scope.picker = (Math.random()*5).floor();
+    $scope.picker = Math.floor(Math.random()*5);
     console.log($scope.picker);
     $scope.imageLeft = $scope.photoArray[$scope.picker];
+    console.log($scope.photoArray[$scope.picker]);
     
-    $scope.imageRight = function(){
-        $scope.picker = (Math.random()*5).floor();
-        return $scope.photoArray[$scope.picker];
-        
-        console.log($scope.photoArray[$scope.picker]);
-    };
+    $scope.picker = Math.floor(Math.random()*5+1);
+    $scope.imageRight = $scope.photoArray[$scope.picker];
     
     $scope.logo = "logoGreen.png";
-    
-    console.log($scope.logo);
 }); 
