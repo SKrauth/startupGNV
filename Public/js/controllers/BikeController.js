c6e4ee8694220be033f2041b22c5b8bb9b9c741c
SKrauth <spkrauth@gmail.com>
2015-10-25 10:08:09 -0400

    minor fixed image quality and controller

--- Public/js/controllers/BikeController.js
+++ Public/js/controllers/BikeController.js
@@ -1,29 +1,31 @@
 app.controller("BikeController", function($scope){
-    $scope.frameImg = "Mountain-Bike-Riding-Lion.jpg";
-    $scope.wheelImg = "Mountain-Bike-Riding-Lion.jpg";
+    $scope.framesImgs = ["Mountain-Bike-Riding-Lion.jpg", "frameOrange.png", "frameGreen.png", "frameYellow.png"];
+    $scope.wheelImgs = ["Mountain-Bike-Riding-Lion.jpg", "wheelOrange.png", "wheelGreen.png", "wheelYellow.png"];
+    $scope.frameImg = $scope.framesImgs[0];
+    $scope.wheelImg = $scope.wheelImgs[0];
     
     $scope.frameOrange = function(){
-        $scope.frameImg = "frameOrange.png"
+        $scope.frameImg = $scope.framesImgs[1];
     };
     
     $scope.frameGreen = function(){
-        $scope.frameImg = "frameGreen.png"
+        $scope.frameImg = $scope.framesImgs[2];
     };
     
     $scope.frameYellow = function(){
-        $scope.frameImg = "frameYellow.png"
+        $scope.frameImg = $scope.framesImgs[3];
     };
     
     
     $scope.wheelOrange = function(){
-        $scope.wheelImg = "wheelOrange.png"
+        $scope.wheelImg = $scope.wheelImgs[1];
     };
     
     $scope.wheelGreen = function(){
-        $scope.wheelImg = "wheelGreen.png"
+        $scope.wheelImg = $scope.wheelImgs[2];
     };
     
     $scope.wheelYellow = function(){
-        $scope.wheelImg = "wheelYellow.png"
+        $scope.wheelImg = $scope.wheelImgs[3];
     };
 });
